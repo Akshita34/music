73419b0a0dcae4a43c318d1b794331ad7e18adab
Akshita34 <124018658+Akshita34@users.noreply.github.com>
2023-06-19 15:14:57 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,97 @@
+const music = new Audio("01 - Bekhayali - Downloadming.SE.mp3");
+let play = document.getElementById("play");
+let line = document.getElementsByClassName("line");
+
+const songs = [
+    {
+        id:'1',
+        songName:` On My Way <br>
+        <div class="subtitle">Alan Walker</div>`,
+        poster: "img/1.jpg"
+    },
+    {
+        id:'2',
+        songName:` Alan Walker-Fade <br>
+        <div class="subtitle">Alan Walker</div>`,
+        poster: "img/2.jpg"
+    },
+    // all object type 
+    {
+        id:"3",
+        songName: `Cartoon - On & On <br><div class="subtitle"> Daniel Levi</div>`,
+        poster: "img/3.jpg",
+    },
+    {
+        id:"4",
+        songName: `Warriyo - Mortals <br><div class="subtitle">Mortals</div>`,
+        poster: "img/4.jpg",
+    },
+    {
+        id:"5",
+        songName: `Ertugrul Gazi <br><div class="subtitle">Ertugrul</div>`,
+        poster: "img/5.jpg",
+    },
+    {
+        id:"6",
+        songName: `Electronic Music <br><div class="subtitle">Electro</div>`,
+        poster: "img/6.jpg",
+    },
+    {
+        id:"7",
+        songName: `Agar Tum Sath Ho <br><div class="subtitle">Tamashaa</div>`,
+        poster: "img/7.jpg",
+    },
+    {
+        id:"8",
+        songName: `Suna Hai <br><div class="subtitle">Neha Kakker</div>`,
+        poster: "img/8.jpg",
+    },
+    {
+        id:"9",
+        songName: `Dilber <br><div class="subtitle">Satyameva Jayate</div>`,
+        poster: "img/9.jpg",
+    },
+    {
+        id:"10",
+        songName: `Duniya <br><div class="subtitle">Luka Chuppi</div>`,
+        poster: "img/10.jpg",
+    },
+    {
+        id:"11",
+        songName: `Lagdi Lahore Di <br><div class="subtitle">Street Dancer 3D</div>`,
+        poster: "img/11.jpg",
+    },
+    {
+        id:"12",
+        songName: `Putt Jatt Da <br><div class="subtitle">Putt Jatt Da</div>`,
+        poster: "img/12.jpg",
+    }
+]
+
+
+
+
+
+
+play.addEventListener("click", ()=>{
+    if (music.paused || music.currentTime <=0){
+        music.play();
+        play.classList.remove("fa-play");
+        play.classList.add("fa-forward-step");
+        line.classList.add("active2");
+    }else{
+        music.pause();
+        play.classList.remove("fa-forward-step");
+        play.classList.add("fa-play");
+        line.classList.remove("active2");
+    }
+})
+
+let index = 0;
+Array.from(document.getElementsByClassName("play1")).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        index = e.target.id;
+        music.src = `${index}.mp3`;
+        music.play();
+    })
+})
